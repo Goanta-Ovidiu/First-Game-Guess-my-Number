26a1d9f198b05b21ed481e6cfca25da0d55ed4dd
Ovidiu Goanta <81975175+Goanta-Ovidiu@users.noreply.github.com>
2021-09-29 17:17:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉 Correct Number!';
 
@@ -9,7 +9,7 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
-
+*/
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
@@ -57,28 +57,28 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
 
-    // When guess is too high
-   } else if (guess > secretNumber) {
-     if (score > 1) {
-       document.querySelector('.message').textContent = '📈 Too high!';
-       score--;
-       document.querySelector('.score').textContent = score;
-     } else {
-       document.querySelector('.message').textContent = '💥 You lost the game!';
-       document.querySelector('.score').textContent = 0;
-     }
+  //   // When guess is too high
+  // } else if (guess > secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = '📈 Too high!';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = '💥 You lost the game!';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
 
-     // When guess is too low
-   } else if (guess < secretNumber) {
-     if (score > 1) {
-       document.querySelector('.message').textContent = '📉 Too low!';
-       score--;
-       document.querySelector('.score').textContent = score;
-     } else {
-      document.querySelector('.message').textContent = '💥 You lost the game!';
-       document.querySelector('.score').textContent = 0;
-     }
-   }
+  //   // When guess is too low
+  // } else if (guess < secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = '📉 Too low!';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = '💥 You lost the game!';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+  // }
 });
 
 document.querySelector('.again').addEventListener('click', function () {
@@ -94,5 +94,3 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
-
-
